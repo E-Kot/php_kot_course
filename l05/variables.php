16fd10fda57f3cb69aa84e5885a5080992dc0bbd
E-Kot <eva.k.web@gmail.com>
2021-06-18 21:48:56 +0300

    l05: Added Variables examples

---
+++ l05/variables.php
@@ -0,0 +1,23 @@
+<?php
+
+$fruit = 'Orange';
+echo $fruit, PHP_EOL;
+echo $fruit, PHP_EOL;
+
+$fruit = 'Apple';
+echo $fruit, PHP_EOL;
+echo $fruit, PHP_EOL;
+echo $fruit, PHP_EOL;
+
+// FAILS:
+// test = 123; $ is required
+// $1test = 123; no numbers on start
+// $te#st = 123; no special characters
+// $te st = 123; no spaces
+
+//   AVAILABLE:
+$tes2t = 123;
+$_test_2v = 123;
+$minSum = 12;
+$allStudentslist = [];
+
