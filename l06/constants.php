28ac47c003f186810f539b9fedbe51aacc107cec
E-Kot <eva.k.web@gmail.com>
2021-09-24 18:12:43 +0300

    l06: Added constants demonstration

---
+++ l06/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+define('APP_VERSION', '3.0.1'); // Нельзя переопределить!
+
+echo APP_VERSION, PHP_EOL;
+
+const POWER = '220V'; // Нельзя переопределить!
+//const POWER = '380V';
+echo POWER, PHP_EOL;
+
+const MY_COEFFICIENT = 3.1;
+$salary = 2000;
+echo $salary * MY_COEFFICIENT, PHP_EOL;
