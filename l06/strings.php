fac2318fa61275895cefd863895279af3e87f21c
E-Kot <eva.k.web@gmail.com>
2021-09-24 17:36:52 +0300

    l06: Added string formats demonstration

--- l06/strings.php
+++ l06/strings.php
@@ -10,3 +10,28 @@ print PHP_EOL;
 
 $f1 = print 1238;
 var_dump($f1); // если все отработало правильно, то вернет 1, если нет - 0 или ошибку.
+
+$name = 'Eva';
+echo 'Hello $name\n', PHP_EOL;
+echo "Hello $name\n";
+echo "Hello, {$name}\n"; // предпочтительный способ вывода
+
+echo "Hello, user with name \"{$name}\"\n"; // Экранирование кавычек или символов
+
+// *************   Вывод многострочного кода:
+// Тут тег в одинарных кавычках. НЕ поддерживает переменные!!!
+$nowdoc = <<<'HTML'
+<script type="text/javascript">
+    alert(1);
+</script>
+HTML;
+
+// Тут тег пишется в двойных кавычках. Поддерживает переменные!!!
+$elementId = 'element1';
+$heredoc = <<<JS
+    document.getElementById('{$elementId}');
+JS;
+
+echo $nowdoc, PHP_EOL, $heredoc, PHP_EOL;
+
+
