88f0849f0c2aac62fde28f6c3220e3748c26550f
E-Kot <eva.k.web@gmail.com>
2021-09-24 16:13:11 +0300

    l05: Added new file l05/my_test.php

---
+++ l05/my_test.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(E_ALL);
+
+$numeral1 = readline('enter the first number: ');
+$numeral2 = readline('enter the second number: ');
+$sum = $numeral1 + $numeral2;
+
+echo "The sum of numbers {$numeral1} and {$numeral2} is {$sum}", PHP_EOL;
