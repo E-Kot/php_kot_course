84f70bfd3987efeb7ae86b7a4cc7078a7dade252
E-Kot <eva.k.web@gmail.com>
2021-09-24 18:42:18 +0300

    l06: Added math operators demonstration

---
+++ l06/math.php
@@ -0,0 +1,24 @@
+<?php
+
+echo 1 + 2, PHP_EOL;
+echo 1 - 2, PHP_EOL;
+echo 1 * 2, PHP_EOL;
+echo 1 / 2, PHP_EOL;
+
+echo 543 * 0.2, PHP_EOL;  // 20%
+
+echo 3 % 2, PHP_EOL; // Остаток от деления
+echo 3 % 5, PHP_EOL;
+echo 8 % 3, PHP_EOL;
+
+echo 3 ** 2, PHP_EOL; // Возвести в степень (3 во 2 степени)
+echo 5 ** 3, PHP_EOL;
+
+$result = 0;
+$result += 2;
+$result += 7;
+$result *= 2;
+$result /= 5;
+echo $result, PHP_EOL;
+
+
